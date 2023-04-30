eed2976d22fc00582083d4a15f56c6e85200b022
GI_Jack <GI_Jack@hackermail.com>
2023-04-29 20:43:01 -0700

    worked in progress, but IRC bouncer added

---
+++ irc-bouncer/README.md
@@ -0,0 +1,6 @@
+IRC Bouncer
+===========
+
+A Bouncer image that features the ZNC IRC Bouncer. If you have a FQDN, then
+certbot will run automaticly, pull Lets Encrypt! certs, and then update the ZNC
+config to run authenticated certificates
---
+++ irc-bouncer/init.local.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+# User initialization script. These commands run in the chroot as it gets
+# compiled.
+
+message() {
+  echo "Cloud Image Init: ${@}"
+}
+submsg(){
+  echo "[+]	${@}"
+}
+warn(){
+  echo 1>&2 "Cloud Image Init: WARN: ${@}"
+}
+
+exit_with_error(){
+  echo 1>&2 "Cloud Image Init: ERROR: ${2}"
+  exit ${1}
+}
+
+main() {
+  # code goes here
+  message "Running user run-once code for intializing template"
+  
+  # fix resolv.conf
+  rm -f /etc/resolv.conf
+  ln -s /usr/lib/systemd/resolv.conf /etc/resolv.conf
+  exit 0
+}
+
+main "${@}"
